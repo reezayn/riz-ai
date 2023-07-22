aa4631552256d17830db6fdcc0070afbe6b50661
Rijan Shrestha <rijanshrestha@Rijans-MacBook-Air.local>
2023-07-23 01:17:25 +0545

    dashboard

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const LandingPage = () => {
   return (
     <>
-      <div>LandingPage (Unprotected)</div>
+      <div>
+        <Link href="/sign-in">
+          <Button>SignIn</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>SignUp</Button>
+        </Link>
+      </div>
     </>
   )
 }
